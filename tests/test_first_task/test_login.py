6c3d4104e84e5a677a2c08ac9092a3ea5ba867aa
Yan Tsiun <tsiun4ick@gmail.com>
2026-05-17 21:50:38 +0700

    Add Playwright Pytest autotests project

---
+++ tests/test_first_task/test_login.py
@@ -0,0 +1,9 @@
+from playwright.sync_api import Page, expect
+
+def test_login(page: Page):
+    page.goto('http://144.31.139.115:5000/')
+    page.get_by_test_id('nav-login').click()
+    page.get_by_test_id('login-username').fill('testDataLogin')
+    page.get_by_test_id('login-password').fill('testDataPassword')
+    page.get_by_role('button', name='Confirm').click()
+    expect(page.get_by_text('Invalid login or password.')).to_be_visible()
