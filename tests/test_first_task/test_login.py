30ea6a2fe329cbdca931d632618bf8dfcbedacbd
Yan Tsiun <tsiun4ick@gmail.com>
2026-05-19 13:28:37 +0700

    Add Faker dependency and update test_login.py to use dynamic credentials

--- tests/test_first_task/test_login.py
+++ tests/test_first_task/test_login.py
@@ -1,10 +1,18 @@
 from playwright.sync_api import Page, expect
 from config import BASE_URL
+from faker import Faker
+
+fake = Faker()
 
 def test_login(page: Page):
     page.goto(BASE_URL)
     page.get_by_test_id('nav-login').click()
-    page.get_by_test_id('login-username').fill('testDataLogin')
-    page.get_by_test_id('login-password').fill('testDataPassword')
+
+    username = fake.user_name()
+    password = fake.password()
+
+    page.get_by_test_id('login-username').fill(username)
+    page.get_by_test_id('login-password').fill(password)
+    
     page.get_by_role('button', name='Confirm').click()
     expect(page.get_by_text('Invalid login or password.')).to_be_visible()
