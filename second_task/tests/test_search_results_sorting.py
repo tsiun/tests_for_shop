8259c1709e59d893ce983aca2674bf0b38e05ad9
Yan Tsiun <tsiun4ick@gmail.com>
2026-05-30 21:11:20 +0700

    refactor: update search results sorting test to use FilterType enum and improve assertions

--- second_task/tests/test_search_results_sorting.py
+++ second_task/tests/test_search_results_sorting.py
@@ -1,16 +1,24 @@
 from playwright.sync_api import Page
 import pytest
-from config import BASE_URL
-from second_task import SearchResultsPage
-from second_task import MainPage
+from second_task.pages import MainPage
+from second_task.pages import SearchResultsPage
+from enum import StrEnum
+from second_task.utils.config_reader import ConfigReader
+
+config = ConfigReader()
+BASE_URL = config.get('BASE_URL')
+
+class FilterType(StrEnum):
+    LOW_TO_HIGH = 'Price: low to high'
+    HIGH_TO_LOW = 'Price: high to low'
 
 @pytest.mark.parametrize(
     'name, n, filter_type',
     [
-        pytest.param(('city', 10, 'Price: low to high'), id='city, 10, Price: low to high'),
-        pytest.param(('city', 15, 'Price: high to low'), id='city, 15, Price: high to low'),
-        pytest.param(('habits', 10, 'Price: low to high'), id='habits, 10, Price: low to high'),
-        pytest.param(('habits', 15, 'Price: high to low'), id='habits, 15, Price: high to low')
+        pytest.param('city', 10, FilterType.LOW_TO_HIGH),
+        pytest.param('city', 15, FilterType.HIGH_TO_LOW),
+        pytest.param('habits', 10, FilterType.LOW_TO_HIGH),
+        pytest.param('habits', 15, FilterType.HIGH_TO_LOW)
     ]
 )
 
@@ -23,6 +31,11 @@ def test_search_result(page: Page, name, n, filter_type):
     search_results = SearchResultsPage(page)
     search_results.filter(filter_type)
 
-    prices = search_results.get_elements_by_filter(n)
+    actual_prices = search_results.get_elements_by_filter(n)
+    expected_prices = sorted(actual_prices)
 
-    assert prices == sorted(prices)
+    if filter_type == FilterType.HIGH_TO_LOW:
+        expected_prices.reverse()
+
+    assert actual_prices == expected_prices, \
+        f"Expected prices to be sorted in {expected_prices} order, but got {actual_prices}"
