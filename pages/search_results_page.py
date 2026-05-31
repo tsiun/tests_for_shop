65a2d2f05688285308d533b2cbf92703c6021281
Yan Tsiun <tsiun4ick@gmail.com>
2026-05-31 17:51:30 +0700

    feat: implement SearchResultsPage class for handling search results and filtering

---
+++ pages/search_results_page.py
@@ -0,0 +1,27 @@
+from playwright.sync_api import Page
+
+class SearchResultsPage:
+    def __init__(self, page: Page):
+        self.page = page
+        self.filter_sort = page.get_by_test_id('filter-sort')
+        self.apply_button = page.get_by_test_id('apply-filters-button')
+        self.prices = page.locator('[data-testid^="search-result-price"]')
+        self.loader = page.get_by_test_id('results-loader')
+        self.table = page.get_by_test_id('results-content')
+
+    def filter(self, filter_type: str):
+        self.filter_sort.select_option(value=filter_type)
+        self.apply_button.click()
+        self.wait_for_open()
+
+    def wait_for_open(self):
+        self.loader.wait_for(state="hidden")
+        self.table.wait_for(state="visible")
+
+    def get_elements_by_filter(self, n: int):
+        prices_row = []
+        for i in range(n):
+            el = self.prices.nth(i)
+            price = int(el.get_attribute('data-price'))
+            prices_row.append(price)
+        return prices_row
