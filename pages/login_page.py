9dba9c44d9e639aa11fcd1d52697f44d2ea17728
Yan Tsiun <tsiun4ick@gmail.com>
2026-06-10 14:52:52 +0700

    refactor LoginPage to inherit from BasePage and update constructor to accept config

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,11 +1,14 @@
 from playwright.sync_api import Page
 from ui.web_element import WebElement
+from ui.base_page import BasePage
 
-class LoginPage:
-
-    def __init__(self, page: Page):
-        self.page = page
+class LoginPage(BasePage):
+    _path = "/basic_auth"
+    _requires_basic_auth = True
+    
+    def __init__(self, page: Page, config: dict) -> None:
+        super().__init__(page, config)
         self.success_message = WebElement(
-            self.page.get_by_test_id('content'),
+            locator = self.page.get_by_test_id('content').locator('p'),
             description="Auth page -> endpoint page",
         )
