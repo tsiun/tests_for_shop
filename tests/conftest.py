66ebcd30e1081dbf57a75774d6a1e353c8b60b3d
Yan Tsiun <tsiun4ick@gmail.com>
2026-06-10 14:53:05 +0700

    refactor conftest.py to streamline fixture definitions and improve login page setup

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,8 @@
 import pytest
 from playwright.sync_api import sync_playwright
 from examples.page_factory_example import PageFactory
-from examples.page_object_example import LoginPage
-from utils.url_utils import embed_credentials_in_url
 from logger import setup_logger
+from pages.login_page import LoginPage
 
 @pytest.fixture(scope="session", autouse=True)
 def init_logger():
@@ -18,43 +17,21 @@ def browser():
         browser.close()
 
 
-@pytest.fixture
-def page(browser):
-    factory = PageFactory.from_json(browser, "config.json")
-    page = factory.create_page()
-    yield page
-    page.close()
+@pytest.fixture(scope="session", autouse=True)
+def ui_factory(browser):
+    return PageFactory.from_json(browser, "config.json")
+
 
 @pytest.fixture
-def login_page(page):
-    url = embed_credentials_in_url(
-        "https://the-internet.herokuapp.com/basic_auth",
-        username="admin",
-        password="admin",
-    )
-    page.goto(url)
-    return LoginPage(page)
+def page(ui_factory):
+    page_context = ui_factory.create_page()
+    yield page_context
+    page_context.close()
 
 
-# @pytest.fixture
-# def login_page(page):
-#     page.goto("/basic_auth")
-#     return LoginPage(page)
+@pytest.fixture
+def login_page(page, ui_factory):
+    page_object = LoginPage(page, config = ui_factory.config)
+    page_object.open()
+    return page_object
 
-
-# @pytest.fixture
-# def page(page_factory):
-#     #           ↓ создаёт BrowserContext + новую вкладку с нужными настройками
-#     return page_factory.create_page()
-
-
-# @pytest.fixture(scope="session")
-# def page_factory(browser):
-#     #      ↓ classmethod читает JSON и возвращает PageFactory(browser, config)
-#     return PageFactory.from_json(browser, "config.json")
-
-
-# @pytest.fixture
-# def login_page(page, page_factory):
-#     #          ↓ передаём page и config dict в BasePage
-#     return LoginPage(page, page_factory.config)
