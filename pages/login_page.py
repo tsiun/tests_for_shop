d4f593d2d6bbf59c8a228b21c37e84e27f619ba4
Yan Tsiun <tsiun4ick@gmail.com>
2026-06-11 19:22:52 +0700

    refactor LoginPage to remove unnecessary imports and constructor

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,14 +1,5 @@
-from playwright.sync_api import Page
-from ui.web_element import WebElement
 from ui.base_page import BasePage
 
 class LoginPage(BasePage):
     _path = "/basic_auth"
     _requires_basic_auth = True
-    
-    def __init__(self, page: Page, config: dict) -> None:
-        super().__init__(page, config)
-        self.success_message = WebElement(
-            locator = self.page.get_by_test_id('content').locator('p'),
-            description="Auth page -> endpoint page",
-        )
