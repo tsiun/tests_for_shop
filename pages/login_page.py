7ebe5b7911077722fb6a9cb1309f06c92843d2f7
Yan Tsiun <tsiun4ick@gmail.com>
2026-06-12 21:42:20 +0700

    remove constructor from LoginPage class

--- pages/login_page.py
+++ pages/login_page.py
@@ -2,5 +2,4 @@ from ui.base_page import BasePage
 from playwright.sync_api import Page
 
 class LoginPage(BasePage):
-    def __init__(self, page: Page, config: dict) -> None:
-        super().__init__(page, config)
+    pass
