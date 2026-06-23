35b7ae5807f8179d892ae528114bc6fc5e109f90
Yan Tsiun <tsiun4ick@gmail.com>
2026-06-23 18:23:03 +0700

    feat: update alert tests to improve clarity and structure

--- tests/test_02_alert_page.py
+++ tests/test_02_alert_page.py
@@ -1,9 +1,30 @@
 import pytest
 from pages.alert_page import AlertPage
+from pages.alert_page import random_word
 
 def test_alert_text(alert_page: AlertPage) -> None:
 
-    actual_message = alert_page.trigger_alert_and_accept()
-    expected_message = 'I am a JS Alert'
+    actual_alert = alert_page.click_alert_and_accept()
+    expected_alert = 'I am a JS Alert'
+    assert actual_alert == expected_alert, f"Expected text: '{expected_alert}', but got: '{actual_alert}'"
 
-    assert actual_message == expected_message, f"Expected text: '{expected_message}', but got: '{actual_message}'"
+    result_alert = alert_page.get_result_text()
+    expected_result_alert = 'You successfully clicked an alert'
+    assert result_alert == expected_result_alert, f"Expected text: '{expected_result_alert}', but got: '{result_alert}'"
+
+    actual_confirm = alert_page.click_confirm_and_accept()
+    expected_confirm = 'I am a JS Confirm'
+    assert actual_confirm == expected_confirm, f"Expected text: '{expected_confirm}', but got: '{actual_confirm}'"
+
+    result_confirm = alert_page.get_result_text()
+    expected_result_confirm = 'You clicked: Ok'
+    assert result_confirm == expected_result_confirm, f"Expected text: '{expected_result_confirm}', but got: '{result_confirm}'"
+
+    actual_prompt = alert_page.click_prompt_and_input()
+    expected_prompt = 'I am a JS prompt'
+    assert actual_prompt == expected_prompt, f"Expected text: '{expected_prompt}', but got: '{actual_prompt}'"
+
+    result_prompt = alert_page.get_result_text()
+    expected_result_prompt = f'You entered: {random_word}'
+    assert result_prompt == expected_result_prompt, f"Expected text: '{expected_result_prompt}', but got: '{result_prompt}'"
+
