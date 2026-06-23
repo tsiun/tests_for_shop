bc094296f983bbbefbdfba884f019c1280ce99a9
Yan Tsiun <tsiun4ick@gmail.com>
2026-06-23 14:35:32 +0700

    chore: remove obsolete test files for alert and login page

--- tests/test_login_page.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from pages.login_endpoint_page import EndpointPage
-
-def test_login_success(endpoint_page: EndpointPage) -> None:
-
-    expected = "Congratulations! You must have the proper credentials."
-    actual = endpoint_page.get_success_message()
-
-    assert actual == expected, f"Expected text: '{expected}', but got: '{actual}'"
