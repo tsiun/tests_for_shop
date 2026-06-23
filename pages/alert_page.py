64e6cfa81d0d85f65991628a61c61606ba261008
Yan Tsiun <tsiun4ick@gmail.com>
2026-06-23 18:22:54 +0700

    feat: enhance AlertPage with structured button handling and result text retrieval

--- pages/alert_page.py
+++ pages/alert_page.py
@@ -3,18 +3,42 @@ from ui.web_element import WebElement
 from logger import setup_logger
 from pages.base_page import BasePage
 from ui.page_actions import PageActions
+from faker import Faker
 
 logger = setup_logger(__name__)
+
+fake = Faker()
+random_word = fake.word()
 
 class AlertPage(BasePage):
     def __init__(self, page: Page, config: dict) -> None:
         super().__init__(page, config)
         self.page_actions = PageActions(page)
-        self.trigger_button = WebElement(
+        self.alert_button = WebElement(
             self.page.get_by_role("button", name="Click for JS Alert"),
-            description="Alert page -> Alert show"
+            description="Alert page -> Alert button"
+        )
+        self.confirm_button = WebElement(
+            self.page.get_by_role("button", name="Click for JS Confirm"),
+            description="Alert page -> Confirm button"
+        )
+        self.prompt_button = WebElement(
+            self.page.get_by_role("button", name="Click for JS Prompt"),
+            description="Alert page -> Prompt button"
+        )
+        self.result_text = WebElement(
+            self.page.locator("#result"),
+            description="Alert page -> text inside result block"
         )
 
-    def trigger_alert_and_accept(self) -> str:
-        return self.page_actions.run_and_accept_alert(self.trigger_button.click)
-
+    def click_alert_and_accept(self) -> str:
+        return self.page_actions.run_and_accept_alert(self.alert_button.click)
+    
+    def click_confirm_and_accept(self) -> str:
+        return self.page_actions.run_and_accept_alert(self.confirm_button.click)
+    
+    def click_prompt_and_input(self) -> str:
+        return self.page_actions.run_and_accept_prompt(self.prompt_button.click, prompt_text=f"{random_word}")
+    
+    def get_result_text(self) -> str:
+        return self.result_text.get_text_content()
