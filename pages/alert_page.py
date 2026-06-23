4f3c2d011da3f7633fab240010f8ef2c532df9bd
Yan Tsiun <tsiun4ick@gmail.com>
2026-06-23 14:35:06 +0700

    refactor: simplify alert handling by utilizing PageActions for dialog interactions

--- pages/alert_page.py
+++ pages/alert_page.py
@@ -2,28 +2,19 @@ from playwright.sync_api import Page
 from ui.web_element import WebElement
 from logger import setup_logger
 from pages.base_page import BasePage
+from ui.page_actions import PageActions
 
 logger = setup_logger(__name__)
 
 class AlertPage(BasePage):
     def __init__(self, page: Page, config: dict) -> None:
         super().__init__(page, config)
+        self.page_actions = PageActions(page)
         self.trigger_button = WebElement(
             self.page.get_by_role("button", name="Click for JS Alert"),
             description="Alert page -> Alert show"
         )
 
     def trigger_alert_and_accept(self) -> str:
-        with self.page.expect_event("dialog") as dialog_info:
-            self.trigger_button.click()
-
-        dialog = dialog_info.value
-        message = dialog.message
-
-        logger.info(f"Перехвачен алерт с текстом: {dialog.message}")
-
-        dialog.accept()
-
-        return message
-
+        return self.page_actions.run_and_accept_alert(self.trigger_button.click)
 
