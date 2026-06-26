2005c46ff5c0ef7bb441c678be1f3d0a383c583a
Yan Tsiun <tsiun4ick@gmail.com>
2026-06-26 20:43:55 +0700

    fix: swap actual and expected values in test_context_menu for clarity

--- tests/test_03_context_menu_page.py
+++ tests/test_03_context_menu_page.py
@@ -1,8 +1,7 @@
-import pytest
 from pages.context_menu_page import ContextMenu
 
 def test_context_menu(context_menu_page: ContextMenu) -> None:
-    actual_context = context_menu_page.make_right_click()
-    expected_context = 'You selected a context menu'
+    expected_context = context_menu_page.make_right_click()
+    actual_context = 'You selected a context menu'
     
     assert actual_context == expected_context, f"Expected text: '{expected_context}', but got: '{actual_context}'"
