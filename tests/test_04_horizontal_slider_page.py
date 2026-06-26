147e84492692b0a118343880013edd0464bf2176
Yan Tsiun <tsiun4ick@gmail.com>
2026-06-26 20:44:13 +0700

    feat: add test for horizontal slider functionality

---
+++ tests/test_04_horizontal_slider_page.py
@@ -0,0 +1,7 @@
+from pages.horizontal_slider_page import HorizontalSlider
+
+def test_horizontal_slider(horizontal_slider_page: HorizontalSlider) -> None:
+    expected_value = horizontal_slider_page.move_slider_randomly()
+    actual_value = horizontal_slider_page.get_result_value()
+
+    assert actual_value ==expected_value, f"Expected text: '{expected_value}', but got: '{actual_value}'"
