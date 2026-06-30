1173af3a940c2d9d90829a14948c499ef3d1bbd3
Yan Tsiun <tsiun4ick@gmail.com>
2026-06-30 16:15:17 +0700

    refactor: streamline slider methods by removing default parameters and adding getter methods for min, max, and step values

--- pages/horizontal_slider_page.py
+++ pages/horizontal_slider_page.py
@@ -3,6 +3,7 @@ from playwright.sync_api import Page
 from ui.web_element import WebElement
 from random import randint
 
+
 class HorizontalSlider(BasePage):
     def __init__(self, page: Page, config: dict) -> None:
         super().__init__(page, config)
@@ -15,25 +16,33 @@ class HorizontalSlider(BasePage):
             description="Horizontal slider page -> Slider value"
         )
 
-    
-    def _calc_random_steps(self, min_val=0.0, max_val=5.0, step=0.5):
+    def get_min_val(self) -> float:
+        return float(self.slider.get_attribute("min"))
+
+    def get_max_val(self) -> float:
+        return float(self.slider.get_attribute("max"))
+
+    def get_step(self) -> float:
+        return float(self.slider.get_attribute("step"))
+
+    def _calc_random_steps(self, min_val: float, max_val: float, step: float):
         total_steps = round((max_val - min_val) / step)
         n_steps = randint(1, total_steps - 1)
         value_of_slider = round(min_val + n_steps * step, 10)
 
         return value_of_slider, n_steps
-    
 
-    def move_slider_randomly(self, min_val=0.0, max_val=5.0, step=0.5) -> float:
+    def move_slider_randomly(self) -> float:
+        min_val = self.get_min_val()
+        max_val = self.get_max_val()
+        step = self.get_step()
         expected_value, steps = self._calc_random_steps(min_val, max_val, step)
-        
+
         self.slider.focus()
         for _ in range(steps):
             self.slider.locator.press("ArrowRight")
 
-
         return expected_value
-
 
     def get_result_value(self) -> float:
         return float(self.result_value.get_inner_text())
