569de7c0566a8ab6eb52fa49a482315dc3eb7f2f
Yan Tsiun <tsiun4ick@gmail.com>
2026-06-30 14:01:08 +0700

    refactor: remove test for login success from test_01_login_page.py

--- tests/test_01_login_page.py
+++
@@ -1,8 +0,0 @@
-from pages.login_endpoint_page import EndpointPage
-
-def test_login_success(endpoint_page: EndpointPage) -> None:
-
-    actual = "Congratulations! You must have the proper credentials."
-    expected = endpoint_page.get_success_message()
-
-    assert actual == expected, f"Expected text: '{expected}', but got: '{actual}'"
