5b553e5d6ffe1552a4f49f3842870a41198950e7
Yan Tsiun <tsiun4ick@gmail.com>
2026-07-01 20:22:20 +0700

    refactor: reorder expected and actual variables for clarity in test cases

--- tests/test_01_base_auth_page.py
+++ tests/test_01_base_auth_page.py
@@ -1,8 +1,9 @@
 from pages.base_auth_page import BaseAuth
 
+
 def test_base_auth_success(base_auth_page: BaseAuth) -> None:
 
-    actual = "Congratulations! You must have the proper credentials."
-    expected = base_auth_page.get_success_message()
+    expected = "Congratulations! You must have the proper credentials."
+    actual = base_auth_page.get_success_message()
 
     assert actual == expected, f"Expected text: '{expected}', but got: '{actual}'"
--- tests/test_02_alert_page.py
+++ tests/test_02_alert_page.py
@@ -1,29 +1,29 @@
 from pages.alert_page import AlertPage
 from pages.alert_page import random_word
 
+
 def test_alert_text(alert_page: AlertPage) -> None:
 
-    expected_alert = alert_page.click_alert_and_accept()
-    actual_alert = 'I am a JS Alert'
+    actual_alert = alert_page.click_alert_and_accept()
+    expected_alert = 'I am a JS Alert'
     assert actual_alert == expected_alert, f"Expected text: '{expected_alert}', but got: '{actual_alert}'"
 
-    expected_result_alert = alert_page.get_result_text()
-    result_alert = 'You successfully clicked an alert'
+    result_alert = alert_page.get_result_text()
+    expected_result_alert = 'You successfully clicked an alert'
     assert result_alert == expected_result_alert, f"Expected text: '{expected_result_alert}', but got: '{result_alert}'"
 
-    expected_confirm = alert_page.click_confirm_and_accept()
-    actual_confirm = 'I am a JS Confirm'
+    actual_confirm = alert_page.click_confirm_and_accept()
+    expected_confirm = 'I am a JS Confirm'
     assert actual_confirm == expected_confirm, f"Expected text: '{expected_confirm}', but got: '{actual_confirm}'"
 
-    expected_result_confirm = alert_page.get_result_text()
-    result_confirm = 'You clicked: Ok'
+    result_confirm = alert_page.get_result_text()
+    expected_result_confirm = 'You clicked: Ok'
     assert result_confirm == expected_result_confirm, f"Expected text: '{expected_result_confirm}', but got: '{result_confirm}'"
 
-    expected_prompt = alert_page.click_prompt_and_input()
-    actual_prompt = 'I am a JS prompt'
+    actual_prompt = alert_page.click_prompt_and_input()
+    expected_prompt = 'I am a JS prompt'
     assert actual_prompt == expected_prompt, f"Expected text: '{expected_prompt}', but got: '{actual_prompt}'"
 
-    expected_result_prompt = alert_page.get_result_text()
-    result_prompt = f'You entered: {random_word}'
+    result_prompt = alert_page.get_result_text()
+    expected_result_prompt = f'You entered: {random_word}'
     assert result_prompt == expected_result_prompt, f"Expected text: '{expected_result_prompt}', but got: '{result_prompt}'"
-
