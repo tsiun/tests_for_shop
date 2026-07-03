bd60b007dd10944f278fbe953b4527eb85019405
Yan Tsiun <tsiun4ick@gmail.com>
2026-07-03 16:06:47 +0700

    refactor: update test_horizontal_slider to use random slider values for validation

--- tests/test_04_horizontal_slider_page.py
+++ tests/test_04_horizontal_slider_page.py
@@ -1,8 +1,14 @@
 from pages.horizontal_slider_page import HorizontalSlider
+from utils.slider_utils import get_random_slider_value
 
 
 def test_horizontal_slider(horizontal_slider_page: HorizontalSlider) -> None:
-    actual_value = horizontal_slider_page.move_slider_randomly()
-    expected_value = horizontal_slider_page.get_result_value()
+    min_val = horizontal_slider_page.get_min_val()
+    max_val = horizontal_slider_page.get_max_val()
+    step = horizontal_slider_page.get_step()
 
-    assert actual_value == expected_value, f"Expected text: '{expected_value}', but got: '{actual_value}'"
+    expected_value = get_random_slider_value(min_val, max_val, step)
+    horizontal_slider_page.move_slider_to_value(target_value=expected_value)
+    actual_value = horizontal_slider_page.get_current_value()
+
+    assert actual_value == expected_value, f"Expected value: '{expected_value}', but got: '{actual_value}'"
