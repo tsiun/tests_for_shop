8eafb71af24029bb228b8fed27f07338933e1d22
Yan Tsiun <tsiun4ick@gmail.com>
2026-07-03 16:06:40 +0700

    refactor: simplify slider movement logic and remove unused methods

--- pages/horizontal_slider_page.py
+++ pages/horizontal_slider_page.py
@@ -25,24 +25,16 @@ class HorizontalSlider(BasePage):
     def get_step(self) -> float:
         return float(self.slider.get_attribute("step"))
 
-    def _calc_random_steps(self, min_val: float, max_val: float, step: float):
-        total_steps = round((max_val - min_val) / step)
-        n_steps = randint(1, total_steps - 1)
-        value_of_slider = round(min_val + n_steps * step, 10)
+    def get_current_value(self) -> float:
+        return float(self.result_value.get_inner_text())
 
-        return value_of_slider, n_steps
-
-    def move_slider_randomly(self) -> float:
+    def move_slider_to_value(self, target_value: float) -> None:
         min_val = self.get_min_val()
-        max_val = self.get_max_val()
+        # max_val = self.get_max_val()
         step = self.get_step()
-        expected_value, steps = self._calc_random_steps(min_val, max_val, step)
+
+        steps_count = round((target_value - min_val) / step)
 
         self.slider.focus()
-        for _ in range(steps):
+        for _ in range(steps_count):
             self.slider.locator.press("ArrowRight")
-
-        return expected_value
-
-    def get_result_value(self) -> float:
-        return float(self.result_value.get_inner_text())
