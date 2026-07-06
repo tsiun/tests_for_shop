55790459f750894f1aa1339f1f5fb1ff6e4d5b91
Yan Tsiun <tsiun4ick@gmail.com>
2026-07-06 19:39:27 +0700

    refactor: rename DynamicContent to DynamicContentPage and update related methods for consistency

--- pages/dynamic_content_page.py
+++ pages/dynamic_content_page.py
@@ -3,40 +3,33 @@ from ui.multi_web_element import MultiWebElement
 from playwright.sync_api import Page
 
 
-class DynamicContent(BasePage):
+class DynamicContentPage(BasePage):
     def __init__(self, page: Page, config: dict):
         super().__init__(page, config)
-        self.dynamic_image = MultiWebElement(
+        self.dynamic_images = MultiWebElement(
             page=self.page,
             locator=self.page.locator(".large-2.columns img"),
             description="Dynamic page -> dynamic images"
         )
 
-    def get_count_img_src(self) -> list[str]:
-        count_imgs = len(self.dynamic_image.all())
-        sourses = []
-
-        for i in range(count_imgs):
-            example = self.dynamic_image.get_attribute("src")
-            sourses.append(example)
-
-        return sourses
+    def get_image_sources(self) -> list[str]:
+        return [img.get_attribute("src") for img in self.dynamic_images.all()]
 
     def has_duplicate_img_src(self) -> bool:
-        check_duplicate = self.get_count_img_src()
-        return len(set(check_duplicate)) < len(check_duplicate)
+        img_sourses = self.get_image_sources()
+        return len(set(img_sourses)) < len(img_sourses)
 
-    def get_duplicate_imgs(self, max_attempts: int = 3) -> bool:
+    def wait_for_duplicate_images(self, max_attempts: int = 3) -> bool:
         for attempt in range(1, max_attempts + 1):
-            if self.has_duplicate_img_src:
-                print(f"Дубликаты найдены на попытке {attempt}")
+            if self.has_duplicate_img_src():
                 return True
 
             self.page.reload()
 
             # self.dynamic_image.wait_for(state="visible")
             # self.page.wait_for_timeout(timeout=5000)
-            self.dynamic_image.first.wait_for(state="visible")
+            # self.dynamic_image.first().wait_for(state="visible")
+            # self.dynamic_image.wait_for_load_state()
+            self.page.wait_for_load_state("load")
 
-        print(f"После {attempt} попыток дубликаты не найдены.")
         return False
