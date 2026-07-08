81ca5ab948d6b862edfc45c61527f3c219a87fb1
Yan Tsiun <tsiun4ick@gmail.com>
2026-07-08 15:46:07 +0700

    feat: implement UploadImagePage class for handling image uploads

---
+++ pages/upload_image_page.py
@@ -0,0 +1,28 @@
+from pages.base_page import BasePage
+from playwright.sync_api import Page
+from ui.web_element import WebElement
+
+
+class UploadImagePage(BasePage):
+    def __init__(self, page: Page, config: dict):
+        super().__init__(page, config)
+        self.image_upload = WebElement(
+            self.page.locator("#file-upload"),
+            description="Upload page -> file upload"
+        )
+        self.submit_button = WebElement(
+            self.page.get_by_role('button', name='Upload'),
+            description="Uploaded file -> submit button"
+        )
+        self.check_file_name = WebElement(
+            self.page.locator("#uploaded-files"),
+            description="Uploaded file -> check file_name"
+        )
+
+    def upload_file(self) -> None:
+        self.image_upload.set_input_files(file_path="utils/lego_batman.jpg")
+
+        self.submit_button.click()
+
+    def check_result(self) -> str:
+        return self.check_file_name.get_inner_text()
