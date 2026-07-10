9ac3acbc78ce4d16f4dbbdfb1b5fe4089fe2e038
Yan Tsiun <tsiun4ick@gmail.com>
2026-07-10 21:08:10 +0700

    refactor: update test_upload_image_page to pass file_path to upload_file method

--- tests/test_10_upload_image.py
+++ tests/test_10_upload_image.py
@@ -2,7 +2,8 @@ from pages.upload_image_page import UploadImagePage
 
 
 def test_upload_image_page(upload_image_page: UploadImagePage):
-    upload_image_page.upload_file()
+    file_path = "utils/lego_batman.jpg"
+    upload_image_page.upload_file(file_path=file_path)
 
     actual = upload_image_page.check_result()
     expected = "lego_batman.jpg"
